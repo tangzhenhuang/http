ab03105788852dbf2538214126d47fe23b64e696
tangzhenhuang <tangzhenhuang@cvte.com>
2025-11-27 14:20:15 +0800

    [modify] support any u16 for status

--- src/status.rs
+++ src/status.rs
@@ -71,10 +71,6 @@ impl StatusCode {
     /// ```
     #[inline]
     pub fn from_u16(src: u16) -> Result<StatusCode, InvalidStatusCode> {
-        if !(100..1000).contains(&src) {
-            return Err(InvalidStatusCode::new());
-        }
-
         NonZeroU16::new(src)
             .map(StatusCode)
             .ok_or_else(InvalidStatusCode::new)
